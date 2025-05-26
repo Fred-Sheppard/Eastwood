45092221958c65cdf3f5bfbb68efbdc4dcb865bc
sloggo <switchbyteyt@gmail.com>
2025-05-26 12:25:46 +0100

    add save and load function to session

--- src/key_exchange/DeviceCommunicationSession.h
+++ src/key_exchange/DeviceCommunicationSession.h
@@ -9,7 +9,10 @@
 #include <sodium.h>
 #include <memory>
 #include <vector>
+#include <string>
 #include <cereal/cereal.hpp>
+#include <cereal/archives/json.hpp>
+#include <fstream>
 
 class DeviceCommunicationSession {
 public:
@@ -18,6 +21,20 @@ public:
     virtual const std::vector<uint8_t>& getSharedSecret() const = 0;
     virtual DoubleRatchet* getRatchet() = 0;
     const std::vector<uint8_t>& getDeviceSessionId() const;
+
+    // Save this session to a file
+    void save(const std::string& filename) const {
+        std::ofstream ofs(filename);
+        cereal::JSONOutputArchive oarchive(ofs);
+        oarchive(*this);
+    }
+
+    // Load this session from a file
+    void load(const std::string& filename) {
+        std::ifstream ifs(filename);
+        cereal::JSONInputArchive iarchive(ifs);
+        iarchive(*this);
+    }
 
     template<class Archive>
     void serialize(Archive& ar) {
