5f565f7d0f17d66452551892cd3b792d06b58772
fred <frederickjssheppard@gmail.com>
2025-05-27 23:38:41 +0100

    Fix memory import for ubuntu

--- src/auth/register_user/register_user.h
+++ src/auth/register_user/register_user.h
@@ -1,6 +1,7 @@
 #ifndef SIGNUP_H
 #define SIGNUP_H
 #include <string>
+#include <memory>
 
 int register_user(const std::string &username, const std::unique_ptr<const std::string> &master_password);
 
