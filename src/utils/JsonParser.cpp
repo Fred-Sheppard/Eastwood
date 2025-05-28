69453347de8353356c71bd9ad20f5433b44f40e1
Dylan Teehan <dylan.teehan@gmail.com>
2025-05-28 13:42:54 +0100

    Add throw catch for http errors to pass to frontend

--- src/utils/JsonParser.cpp
+++ src/utils/JsonParser.cpp
@@ -2,6 +2,20 @@
 
 namespace webwood {
 
+std::string extract_response_body(const std::string& response) {
+    size_t json_start = response.find("\r\n\r\n");
+    if (json_start == std::string::npos) {
+        json_start = response.find("\n\n");
+        if (json_start == std::string::npos) {
+            return response;  // Return full response if no body separator found
+        }
+        json_start += 2;
+    } else {
+        json_start += 4;
+    }
+    return response.substr(json_start);
+}
+
 int parse_status_code(const std::string& first_line) {
     size_t first_space = first_line.find(' ');
     if (first_space == std::string::npos) {
@@ -22,7 +36,7 @@ int parse_status_code(const std::string& first_line) {
 
 nlohmann::json parse_json_response(const std::string& response) {
     if (response.empty()) {
-        return {{"data", ""}, {"content_type", "text"}, {"status_code", 0}};
+        throw HttpError("Empty response received", 0);
     }
 
     // Extract status code from the first line
@@ -31,29 +45,25 @@ nlohmann::json parse_json_response(const std::string& response) {
         first_line_end = response.find("\n");
     }
     if (first_line_end == std::string::npos) {
-        return {{"data", response}, {"content_type", "text"}, {"status_code", 0}};
+        throw HttpError("Invalid response format", 0);
     }
 
     std::string first_line = response.substr(0, first_line_end);
     int status_code = parse_status_code(first_line);
 
-    size_t json_start = response.find("\r\n\r\n");
-    if (json_start == std::string::npos) {
-        json_start = response.find("\n\n");
-        if (json_start == std::string::npos) {
-            return {{"data", response}, {"content_type", "text"}, {"status_code", status_code}};
-        }
-        json_start += 2;
-    } else {
-        json_start += 4;
+    // If status code indicates an error (4xx or 5xx), throw HttpError
+    if (status_code >= 400) {
+        throw HttpError(extract_response_body(response), status_code);
     }
+
+    std::string body = extract_response_body(response);
 
     // Extract and parse the JSON body
     try {
-        nlohmann::json body_json = nlohmann::json::parse(response.substr(json_start));
+        nlohmann::json body_json = nlohmann::json::parse(body);
         return {{"data", body_json}, {"content_type", "json"}, {"status_code", status_code}};
     } catch (const nlohmann::json::parse_error&) {
-        return {{"data", response.substr(json_start)}, {"content_type", "text"}, {"status_code", status_code}};
+        return {{"data", body}, {"content_type", "text"}, {"status_code", status_code}};
     }
 }
 
