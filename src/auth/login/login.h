5cff2d4a561774cc11d664069f685036943fb33b
fred <frederickjssheppard@gmail.com>
2025-05-31 22:24:26 +0100

    Fix

--- src/auth/login/login.h
+++ src/auth/login/login.h
@@ -6,6 +6,7 @@
 #ifndef AUTH_LOGIN_H
 #define AUTH_LOGIN_H
 #include <string>
+#include <memory>
 
 void login_user(const std::string &username, const std::unique_ptr<const std::string> &master_password);
 
